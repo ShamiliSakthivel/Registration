47d8045e9301b5cfa6df607d9c5a59eca7b80c2b
BABU S <BABU S@DESKTOP-IS6T1F1>
2024-01-26 14:45:30 +0530

    Add project files.

---
+++ Win.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Registration
+{
+    /// <summary>
+    /// Interaction logic for Win.xaml
+    /// </summary>
+    public partial class Win : Window
+    {
+        public Win()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlogin_Click(object sender, RoutedEventArgs e)
+        {
+            Online online = new Online();
+            online.Show();
+        }
+    }
+}
